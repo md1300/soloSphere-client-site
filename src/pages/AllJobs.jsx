9b5221a600e32551bebe954ed920ebe2fbc56158
md1300 <md.tawhid.khan1998@gmail.com>
2024-10-06 17:59:51 +0600

    handle search field and reset button

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -12,24 +12,26 @@ const [count,setCount]=useState(0)
 const [filter,setFilter]=useState('')
 const [sort,setSort]=useState('')
 const [jobs,setJobs]=useState([])
+const [search,setSearch]=useState('')
+const [searchText,setSearchText]=useState('')
  
     useEffect(()=>{
         const getData=async()=>{
-          const {data}=await axios(`${import.meta.env.VITE_API_URL}/all-jobs?page=${currnetPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}`)
+          const {data}=await axios(`${import.meta.env.VITE_API_URL}/all-jobs?page=${currnetPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`)
           setJobs(data)
       
         }
         getData()
-      },[currnetPage,itemsPerPage,filter,sort])
+      },[currnetPage,itemsPerPage,filter,sort,search])
 
     useEffect(()=>{
         const getData=async()=>{
-          const {data}=await axios(`${import.meta.env.VITE_API_URL}/jobs-count?filter=${filter}`)
+          const {data}=await axios(`${import.meta.env.VITE_API_URL}/jobs-count?filter=${filter}&search=${search}`)
           
           setCount(data.count)
         }
         getData()
-      },[filter])
+      },[filter,search])
 
       
 const numberOfPages=Math.ceil(count/itemsPerPage)
@@ -44,7 +46,16 @@ const handlePaginationPage=value=>{
 const handleResetButton =()=>{
   setFilter('')
   setSort('')
+  setSearch('')
+  setSearchText('')
 }
+
+const handleSearch=e=>{
+  e.preventDefault()
+  // const text=e.target.search.value;
+  setSearch(searchText)
+}
+
 
   return (
     <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
@@ -67,12 +78,14 @@ const handleResetButton =()=>{
             </select>
           </div>
 
-          <form>
+          <form onSubmit={handleSearch}>
             <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
               <input
                 className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
                 type='text'
                 name='search'
+                onChange={(e)=>{setSearchText(e.target.value)}}
+                value={searchText}
                 placeholder='Enter Job Title'
                 aria-label='Enter Job Title'
               />
